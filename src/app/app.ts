f00282011913576c00173ed3517d6ade8da74e14
Ashiq-jr <ashiqjr7@gmail.com>
2025-09-09 23:50:36 +0530

    day 2 - Added notes as comments and introduced interpolation

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { FirstComponent } from "./first-component/first-component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, FirstComponent],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  selector: 'app-root', //The custom HTML tag to represent this component
+  // standalone: true, //Standalone components can be used without being declared in an NgModule
+  imports: [RouterOutlet, FirstComponent], //Imports array is used to import other components, directives, and pipes that this component depends on.
+  templateUrl: './app.html', //Path to the HTML file for this component's template
+  styleUrl: './app.css' //Path to the CSS file for this component's styles
 })
 export class App {
-  protected title = 'myAngularProject';
+  protected title = 'myAngularProject'; //A property to hold the title of the application. using interpolation to display it in the html file
+  a = 10; // class member / property
+  b = 27; // class member / property
+  sum = this.a + this.b; // 'this' keyword is used to access the properties of the class.
 }
